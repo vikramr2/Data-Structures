c058cbd54a1b787ffc0153c3b28656aace284f66
Vikram Ramavarapu <Ramaavi2019@gmail.com>
2020-06-03 18:05:34 -0500

    Implemented Circular List

---
+++ linked-list/circular/LinkedList.h
@@ -0,0 +1,71 @@
+#include <iostream>
+
+using namespace std;
+
+template<typename T>
+class LinkedList {
+	public:
+		struct Node {
+			T elem;
+			Node* next;
+		};
+		LinkedList() : cursor(NULL) {}
+		~LinkedList() { while(!empty()) remove(); }
+		bool empty() const { return !cursor; }
+		
+		/*We define the back as the cursor
+		  This means the front is cursor->next!*/
+		const T& back() const { return cursor->elem; }
+		const T& front() const { return cursor->next->elem; }
+		
+		/*Advance the cursor to the next node T
+		  This essentially rotates the list*/
+		void advance() { cursor = cursor->next; }
+		
+		void add(const T& e) {
+			//initialize node
+			Node* n = new Node;
+			n->elem = e;
+			
+			//if list is empty, set cursor to this
+			if (!cursor) {
+				n->next = n;
+				cursor = n;
+			}
+			
+			//otherwise insert after cursor
+			else {
+				n->next = cursor->next;
+				cursor->next = n;
+			}
+		}
+		
+		void remove() {
+			Node* n = cursor->next;
+			if (n == cursor) { 
+				//is this the only node?
+				cursor = NULL;
+				
+			//if not, shift cursor and delete old node
+			} else {
+				cursor->next = n->next;
+			}
+			delete n;
+		}
+			
+		Node* cursor;
+};
+
+template<typename T>
+ostream& operator<<(ostream& os, const LinkedList<T>& l) {
+	bool cycled = false;
+	typename LinkedList<T>::Node* cur = l.cursor->next;
+	while(!cycled) {
+		os << cur->elem << ' ';
+		cur = cur->next;
+		if (cur == l.cursor->next) {
+			cycled = true;
+		}
+	}
+	return os;
+}
---
+++ linked-list/circular/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "LinkedList.h"
+
+using namespace std;
+
+int main() {
+	LinkedList<int>* l = new LinkedList<int>;
+	l->add(1);
+	l->add(2);
+	l->add(3);
+	l->add(4);
+	cout << *l << endl;
+	l->advance();
+	cout << *l << endl;
+	l->remove();
+	cout << *l << endl;
+	delete l;
+}
